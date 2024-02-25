813e5f7588a68798283d485083ae73fccbe12d97
Hari Shanker Yadav <113604005+Harishankeryadav@users.noreply.github.com>
2024-02-26 00:56:36 +0530

    created service layer

---
+++ src/services/city_service.js
@@ -0,0 +1,54 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService {
+    constructor() {
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity({ data }) {
+        try {
+            const city = await this.cityRepository.createCity(data)
+            return city;
+
+        } catch (error) {
+            console.log("something went wrong in city service layer");
+            throw { error };
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const response = await this.CityRepository.deleteCity(cityId);
+            return response;
+        }
+        catch (error) {
+            console.log("something went wrong in city service layer");
+            throw { error };
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await this.CityRepository.updateCity(cityId, data);
+            return city;
+        }
+        catch (error) {
+            console.log("something went wrong in city service layer");
+            throw { error };
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await this.CityRepository.getCity(cityId);
+            return city;
+        }
+        catch (error) {
+            console.log("something went wrong in city service layer");
+            throw { error };
+        }
+    }
+
+}
+
+module.exports = CityService;
