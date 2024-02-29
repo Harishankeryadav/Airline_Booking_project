bba3ba2d738e3a71c0ac493b6181dae939763a4b
Hari Shanker Yadav <113604005+Harishankeryadav@users.noreply.github.com>
2024-02-29 19:32:21 +0530

    created airport mui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const  {PORT}  = require("./config/serverconfig.js");
 const ApiRoutes = require('./routes/index');
 
+const{Airport, City} = require('./models/index.js');
+const db = require('./models/city')
+
 const CityRepository = require(('./repository/City_repository.js'))
 
 const setupAndStartServer = async () => {
@@ -14,11 +17,22 @@ const setupAndStartServer = async () => {
 
     app.use('/api',ApiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         
         console.log(`server started at ${PORT}`);
-        const repo = new CityRepository();
-        repo.createCity({ name: "acxv" });
+        // const repo = new CityRepository();
+        // repo.createCity({ name: "e" });
+        // const city = await City.findOne({
+        //     where:{
+        //         id:9
+        //     }
+        // });
+        // const airports  = await city.getAirports();
+        // console.log(city, airports);
+
+        // if(process.env.SYNC_DB){
+        //     db.sequelize.sync({alter:true});
+        // }
     });
 }
 
